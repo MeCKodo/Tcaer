ada3176f10f60aeeb74287b85594ee22d9158469
kodo <524580860@qq.com>
2018-09-01 10:20:52 +0800

    improve

--- src/tcaer-dom/utils.js
+++ src/tcaer-dom/utils.js
@@ -4,13 +4,13 @@ function isChange(newNode, oldNode) {
     newNode.type !== oldNode.type
 }
 
-function setAttributes(dom, name, value = '') {
+function setAttributes(node, name, value = '') {
   if (name === 'className') name = 'class';
   
   // event
   if (/on\w+/.test(name)) {
     name = name.toLowerCase();
-    dom[name] = value;
+    node[name] = value;
     return;
   }
   
@@ -18,12 +18,12 @@ function setAttributes(dom, name, value = '') {
   if (name === 'style') {
     Object.keys(value).forEach((item) => {
       const styleValue = value[item];
-      dom.style[item] = typeof styleValue === 'number' ? `${styleValue}px` : styleValue;
+      node.style[item] = typeof styleValue === 'number' ? `${styleValue}px` : styleValue;
     });
     return;
   }
   
-  dom.setAttribute(name, value);
+  node.setAttribute(name, value);
   
 }
 
